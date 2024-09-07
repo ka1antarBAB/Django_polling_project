547ed6350a00db33ee7ef3ac5421b569e746ce1f
ehsan malek <EhsanMalekBS@gmail.com>
2024-09-07 10:35:10 +0330

    static root specified in the settings file

--- polling/settings.py
+++ polling/settings.py
@@ -129,9 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
